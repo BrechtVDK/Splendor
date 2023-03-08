debb693e6e9fefe44d2fb1c1f96d24cb3915801f
DAVID DE GROOTE <david.degroote@student.hogent.be>
2023-03-08 20:49:57 +0100

    de mapper en connection zijn gemaakt.

---
+++ src/persistentie/ConnectieDB.java
@@ -0,0 +1,7 @@
+package persistentie;
+
+public class ConnectieDB {
+
+	public static final String CONNECTIE_STRING = "jdbc:mysql://ID399244_g119.db.webhosting.be?serverTimezone=UTC&useLegacyDatetimeCode=false&user=ID399244_g119&password=YW#U4WDKB43iyxs7";
+
+}
--- src/persistentie/SpelerMapper.java
+++ src/persistentie/SpelerMapper.java
@@ -1,5 +1,33 @@
 package persistentie;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import domein.Speler;
+
 public class SpelerMapper {
+
+	public List<Speler> geefSpelers() {
+		List<Speler> spelers = new ArrayList<>();
+
+		try (Connection conn = DriverManager.getConnection(ConnectieDB.CONNECTIE_STRING);
+				PreparedStatement query = conn.prepareStatement("SELECT * FROM ID399244_g119.Speler");
+				ResultSet rs = query.executeQuery()) {
+
+			while (rs.next()) {
+				String gebruikersnaam = rs.getString("gebruikersnaam");
+				int geboortejaar = rs.getInt("geboortejaar");
+				spelers.add(new Speler(gebruikersnaam, geboortejaar));
+			}
+		} catch (SQLException ex) {
+			throw new RuntimeException(ex);
+		}
+		return spelers;
+	}
 
 }
