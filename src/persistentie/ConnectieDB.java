8d400f226820e09b2ccf871d4a249315db197e91
DAVID DE GROOTE <david.degroote@student.hogent.be>
2023-03-11 14:42:05 +0100

    JavaFX, JUnit5 referentie toegevoegd aan moduel

--- src/persistentie/ConnectieDB.java
+++ src/persistentie/ConnectieDB.java
@@ -2,6 +2,7 @@ package persistentie;
 
 public class ConnectieDB {
 
+//	public static final String CONNECTIE_STRING = "jdbc:mysql://ID399244_g119.db.webhosting.be?serverTimezone=UTC&useLegacyDatetimeCode=false&user=ID399244_g119&password=testSplendor1";
 	public static final String CONNECTIE_STRING = "jdbc:mysql://ID399244_g119.db.webhosting.be?serverTimezone=UTC&useLegacyDatetimeCode=false&user=ID399244_g119&password=testSplendor1";
 
 }
