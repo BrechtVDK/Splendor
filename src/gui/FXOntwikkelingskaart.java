f2c57d20f444581cf3e6f39c65e236921db38c8f
Brecht Vandekerckhove <brecht.vandekerckhove@student.hogent.be>
2023-03-19 23:22:44 +0100

    Achtergrond ontwikkelingskaart toegevoegd

--- src/gui/FXOntwikkelingskaart.java
+++ src/gui/FXOntwikkelingskaart.java
@@ -8,23 +8,25 @@ public class FXOntwikkelingskaart extends FXKaart implements Clickable {
 
 	private Edelsteenfiche bonus;
 
-
 	public FXOntwikkelingskaart(Ontwikkelingskaart info) {
 		super(info.prestigePunten(), info.edelsteenfiches());
 		bonus = info.bonus();
 		buildExtras();
-
 	}
 
 	private void buildExtras() {
 		plaatsBonus();
+		String rgb = bonus.edelsteen().getRgb();
 		this.getStyleClass().add("ontwikkelingskaart");
+		// achtergrond instellen adhv kleur bonus. Witte bonus => rgb(192,192,192)
+		this.setStyle(String.format("-fx-background-color: linear-gradient(rgba(255,255,255), rgba%s);",
+				rgb.equals("(255,255,255)") ? "(192,192,192)" : rgb));
+
 		// interface
 		this.backgroundProperty();
 		this.setOnMouseClicked(this::onClicked);
 		this.onHovered(this, 1.1);
 	}
-
 
 	private void plaatsBonus() {
 		FXEdelsteenFiche fxEdelsteenFiche = new FXEdelsteenFiche(bonus.edelsteen(), 15);
@@ -37,6 +39,5 @@ public class FXOntwikkelingskaart extends FXKaart implements Clickable {
 	public void onLeftClicked() {
 		System.out.println("Ontwikkelingskaart");
 	}
-
 
 }
