12ed69ce235fa483e900e4f14e6e7f6e2fc4f99f
Brecht Vandekerckhove <brecht.vandekerckhove@student.hogent.be>
2023-03-30 19:27:18 +0200

    ingekort door binding in FXEdelsteenfiche

--- src/gui/EdelsteenFicheScherm.java
+++ src/gui/EdelsteenFicheScherm.java
@@ -1,18 +1,14 @@
 package gui;
 
-import java.util.Map;
-
 import domein.DomeinController;
 import domein.Edelsteen;
 import javafx.scene.layout.VBox;
 
 public class EdelsteenFicheScherm extends VBox {
 	private DomeinController dc;
-	private Map<Edelsteen, Integer> aantalFichesPerStapel;
 
 	public EdelsteenFicheScherm(DomeinController dc) {
 		this.dc = dc;
-		this.aantalFichesPerStapel = dc.geefAantalFichesPerStapel();
 		buildGui();
 	}
 
@@ -20,7 +16,7 @@ public class EdelsteenFicheScherm extends VBox {
 		this.setSpacing(10);
 
 		for (Edelsteen e : Edelsteen.values()) {
-			int aantal = aantalFichesPerStapel.get(e);
+			int aantal = dc.geefAantalFichesPerStapel().get(e);
 			FXEdelsteenFiche fxEdelsteenFiche = new FXEdelsteenFicheKlikbaar(e, 38, aantal);
 			this.getChildren().add(fxEdelsteenFiche);
 		}
